c69586100ae7121b7f6445d9ccd2367a6364cc04
KunalSisodia <kunal.17sisodia@gmail.com>
2021-05-09 20:52:35 +0200

    Update semifinal.py

--- spiders/semifinal.py
+++ spiders/semifinal.py
@@ -1,107 +1,92 @@
+
 import scrapy
 from scrapy.http import FormRequest,JsonRequest
 import urllib
 import json
 from scrapy import Item, Field, Request, Spider
+import json
+from lxml import etree
+from scrapy.selector import Selector 
+from scrapy.http import HtmlResponse
 from bs4 import BeautifulSoup
 
+#from demo_project.items import JokeItem
+#from scrapy.loader import ItemLoader
+
+title_1 = []
+object1 = []
+desc_yes = []
+title2 = []
+desc_no = []
+title = []
+list_A = []
+list_B = []
+list_C = []
+list_D = []
 start_urls = []
 debate_id = []
-pro_argument = []
-cons_argument = []
+i = 0
 k = 0
-scraped_info = {}
-pro_title = []
-cons_title = []
+j = True
+l = 0
+m = 0
 pagenumber = 2
 
 class DebateSpider(scrapy.Spider):
-    name= 'semifinal'
+    name= 'second_link'
 
+    # start_urls = [
+    # 'https://www.debate.org/opinions/?sort=popular'
+    # #taking the popular category URL
+    # ]
     start_urls = [
-    'https://www.debate.org/opinions/?sort=popular'
-    #taking the popular category URL
+    'https://www.debate.org/opinions/do-you-agree-with-the-black-lives-matter-movement-1'
     ]
 
     def parse(self, response):
 
-        global k,start_urls,pro_argument,cons_argument,scraped_info,pro_title,cons_title,pagenumber
-            
-        if k == 0:
-            # only for 1 time  
-            
-            for debate in response.xpath("/html/body/div[2]/div[4]/div[1]/div/div/ul/li"):
-                title1 = debate.xpath("/html/body/div[2]/div[4]/div[1]/div/div/ul/li/span/p/a/@href").extract()
-                # Extracting the values in popular catrgories so as to fetch data for top5 opinions
-                object1 = response.xpath("/html/body/div[2]/div[4]/div[1]/div/div/ul/li/@did").extract()
-                            
-            for i in range(6):
-                str = 'https://www.debate.org' + title1[i]
-                # print(str)
-                start_urls.append(str)
-                debate_id.append(object1[i].upper())
-                # creating the URLs for scrappying data
-            
-            k = k + 1
-            print("k ki value after plus 1 ",k)
-            yield response.follow(start_urls[k-1], callback = self.parse)   
+        global title1,k,start_urls,m,l,list_A,list_B
 
-        if k != 0:
-            #  for parsing data from each url
-            scraped_info = {}
-            print("first section k ki value ", k)
-
-            page_title = response.css('span.q-title::text').get()
-
-            for annonce in response.css('.hasData'):
-                strobject_yes = ""
-                strobject_yes = "{{title : \"{}\" , body : {}}}".format(annonce.css('h2::text').extract_first(),annonce.css('p::text').extract_first())
-                pro_argument.append(strobject_yes)
-
-            for debate in response.xpath("/html/body/div[2]/div[4]/div/div/div/div[4]/div[2]/ul/li"):
-                    # No
-                strobject_No = ""
-                strobject_No = "{{ title : \"{}\" , body : {}}}".format(debate.xpath('h2/a/text()').extract_first(),debate.xpath('p/text()').extract_first())
-                cons_argument.append(strobject_No)
+        for debate in response.xpath("/html/body/div[2]/div[4]/div/div/div/div[4]/div[1]/ul/li"):
+                # yes ke liye
+                title_1 = debate.xpath('.//@href').extract()
+                list_A.append(title_1)
+                desc_yes = debate.xpath('p/text()').extract()
+                list_B.append(desc_yes)
             
-            if response.xpath("/html/body/div[2]/div[3]/a[3]/text()") is not None:
-                main_category = response.xpath('/html/body/div[2]/div[3]/a[3]/text()').extract_first()
+                # print (len(list_A), len(list_B))
 
-            # to call other pages
-            body_object = "{{debateId: \"{}\" , pageNumber: {}, itemsPerPage: 10, ysort: 5, nsort: 5 }}".format(debate_id[k-1],pagenumber)
-            
-            yield scrapy.Request(url='https://www.debate.org/opinions/~services/opinions.asmx/GetDebateArgumentPage',
+            # load more arguments
+            # body_object = "{{debateId: \"{}\" , pageNumber: 2, itemsPerPage: 10, ysort: 5, nsort: 5 }}".format(debate_id[k-1])
+            # print(body_object)
+            # print("-************")
+
+        # response = HtmlResponse(url='https://www.debate.org/opinions/~services/opinions.asmx/GetDebateArgumentPage',
+        #                     # method='POST',
+        #                     body='{debateId: "DF5F0C8D-BDA6-4C05-9C50-07FCD527D8BE", pageNumber: "2", itemsPerPage: "10", ysort: "5", nsort: "5"}',
+        #                     headers={'X-Requested-With': 'XMLHttpRequest', 'Content-Type': 'application/json; charset=UTF-8'})
+        
+        # Selector(response = response).xpath('//span/text()').extract()
+
+        yield scrapy.Request(url='https://www.debate.org/opinions/~services/opinions.asmx/GetDebateArgumentPage',
                             method='POST',
-                            body=body_object,
+                            body='{debateId: "DF5F0C8D-BDA6-4C05-9C50-07FCD527D8BE", pageNumber: "2", itemsPerPage: "10", ysort: "5", nsort: "5"}',
                             headers={'X-Requested-With': 'XMLHttpRequest', 'Content-Type': 'application/json; charset=UTF-8'},callback=self.parse_tag)
-
-            scraped_info = {
-            'topic' : page_title, 
-            'category': main_category,
-            'pro_arguments': pro_argument,
-            'con_arguments': cons_argument
-            }
-            
-            if k > 1:
-                yield scraped_info
-            
-        if k <= 4:
-            # It will create new URL for diff opinions
-            k = k + 1
-            yield response.follow(start_urls[k-1], callback = self.parse)
-            # Calling the 5 URL one by one
+        #  change page number in body to get diff pages data
 
 
     def parse_tag(self, response):
 		        #retrieve the tag name
 
-        global pagenumber,pro_title,cons_title,pagenumber
-        pro_title = []
-        cons_title = []
-
+        global pagenumber        
         data = ""
+        print("lalalalalalalla1222222")
         data = json.loads(response.text)
         object_new = data['d']
+
+        # print("**************")
+        # print(object_new)
+        # print("^^^^^^^^^^^^^^^^^^^^")
 
         x = object_new.split("ddo.split")
 
@@ -109,30 +94,70 @@ class DebateSpider(scrapy.Spider):
         soup_yes = BeautifulSoup(x[0], 'html.parser')
         soup_no = BeautifulSoup(x[1], 'html.parser')
         # soup = BeautifulSoup(object_new, "html.parser")
-        for el_yes in soup_yes.find_all("h2"):
-            pro_title.append(el_yes.get_text())
 
-        for el_no in soup_no.find_all("h2"):
-            cons_title.append(el_yes.get_text())
+        for el_p in soup_yes.find_all("p"):
+            print(el_p.get_text())
 
-        for el_p_yes in soup_yes.find_all("p"):
-            strobject_yes = "{{title : \"{}\" , body : {}}}".format(pro_title,el_p_yes.get_text())
-            pro_argument.append(strobject_yes)
+        # for el in soup_yes.find_all("h2"):
+        #     print(el.get_text())
 
-        for el_p_no in soup_yes.find_all("p"):
-            strobject_No = "{{ title : \"{}\" , body : {}}}".format(cons_title,el_p_no.get_text())
-            cons_argument.append(strobject_No)
-            # print(el_p.get_text())
+        # for el_no in soup_no.find_all("h2"):
+        #     print(el_no.get_text())
 
         # if x[2] == 'needmore':
         if "needmore" in x[2]:
 
             pagenumber = pagenumber + 1
-            body_object = "{{debateId: \"{}\" , pageNumber: {}, itemsPerPage: 10, ysort: 5, nsort: 5 }}".format(debate_id[k-1],pagenumber)
+            body_object = "{{debateId: \"DF5F0C8D-BDA6-4C05-9C50-07FCD527D8BE\" , pageNumber: {}, itemsPerPage: 10, ysort: 5, nsort: 5 }}".format(pagenumber)
             print(body_object)
             yield scrapy.Request(url='https://www.debate.org/opinions/~services/opinions.asmx/GetDebateArgumentPage',
                                 method='POST',
+                                # body='{debateId: "DF5F0C8D-BDA6-4C05-9C50-07FCD527D8BE", pageNumber: \"{}\", itemsPerPage: "10", ysort: "5", nsort: "5"}'),
                                 body=body_object,
                                 headers={'X-Requested-With': 'XMLHttpRequest', 'Content-Type': 'application/json; charset=UTF-8'},callback=self.parse_tag)
-        else:
-            pagenumber = 2
+        
+        # for el1 in soup.find_all("p"):
+        #     print(el1.get_text())
+
+        # print(response.meta)
+        # tag_name = response.meta['depth']
+        # print(tag_name)
+        # #use css function to parse the html and find the tags that contains the quotes
+        # quotes_html_tags = response.css('.col-md-8 .quote')
+        # for quote_html_tag in quotes_html_tags:
+        #     #retrieve the text of the tag
+        #     quote_txt = quote_html_tag.css('.text ::text').get()
+        #     #return a tag object
+        #     # yield {"tag": tag_name, "text": quote_txt}	
+
+    def parse_new(self, response):
+
+        print("bhai new function hai ye")
+        data = json.loads(response.text)
+        # print(data)
+        sel = Selector(xml_response)
+        print(json.dumps(data, sort_keys=True, indent=4))
+
+        for debate in response.xpath("/html/body/div[2]/div[4]/div/div/div/div[4]/div[1]/ul/li"):
+            # yes ke liye
+            print("Yes ki liye")
+            title_1 = debate.xpath('.//@href').extract()
+            list_A.append(title_1)
+            desc_yes = debate.xpath('p/text()').extract()
+            list_B.append(desc_yes)
+        
+            print (len(list_A), len(list_B))
+
+        # res = ""
+        # for key in data.keys() :
+        #     res.append(test_dict[key])
+
+        # print("The list of values is : " +  str(res))
+        # selector = scrapy.Selector(text=data['response'], type="html")
+        # print(selector)
+        # obj = selector.xpath('//title/text()').extract()
+        # htmlText = json.loads(response.text)['htmlinjson']
+        # resultPage = etree.HTML(htmlText)
+        # obj = selector.xpath('//title/text()').extract()
+        # print (resultPage.xpath('/html/body/div[2]/div[4]/div/div/div/div[4]/div[2]/ul/li/h2/a/text()'))
+        # print(obj)
